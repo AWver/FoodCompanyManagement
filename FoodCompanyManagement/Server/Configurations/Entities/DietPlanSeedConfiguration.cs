5bb2ca3a6252bbfa68366b80f14c8f5ce6c72b92
AWver <2002685b@student.tp.edu.sg>
2021-12-25 14:56:30 +0800

    SeedConfig Part1

---
+++ FoodCompanyManagement/Server/Configurations/Entities/DietPlanSeedConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FoodCompanyManagement.Server.Configurations.Entities
+{
+	public class DietPlanSeedConfiguration : IEntityTypeConfiguration<DietPlan>
+	{
+		public void Configure(EntityTypeBuilder<DietPlan> builder)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
