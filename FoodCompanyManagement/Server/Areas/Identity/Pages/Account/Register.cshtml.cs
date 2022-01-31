835a29d5d2eef52e342b10fe35638bce8c5446aa
AWver <2002685b@student.tp.edu.sg>
2022-01-31 15:05:07 +0800

    Logging in fixed

--- FoodCompanyManagement/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FoodCompanyManagement/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -90,7 +90,7 @@ namespace FoodCompanyManagement.Server.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { Email = Input.Email, UserName = Input.UserName };
+                var user = new ApplicationUser { UserName = Input.UserName,Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
